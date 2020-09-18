78fa52a362885009fb60f04f3e5832936efca90a
Melchizedek Tetteh <Tettehmel@gmail.com>
2020-09-18 18:36:25 -0400

    Added cocoa pods completed assignment

--- Flixer/MoviesViewController.swift
+++ Flixer/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
@@ -61,11 +62,13 @@ class MoviesViewController: UIViewController,UITableViewDataSource,UITableViewDe
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
         
-        let baseUrl = "http://image.tmdb.org/t/p/"
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
         
         let posterUrl = URL(string: baseUrl + posterPath)
         
+        
+        cell.posterView.af_setImage(withURL: posterUrl!)
         return cell
     }
     
