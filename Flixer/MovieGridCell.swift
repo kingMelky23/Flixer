a8c6f0d53bc6d70b9f48b4ba9f12cd79ee0c24f6
Melchizedek Tetteh <Tettehmel@gmail.com>
2020-09-23 18:55:36 -0400

    Completed  Assignment 2 features: render movie list, view movie details. render all super hero movies, tabs

---
+++ Flixer/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixer
+//
+//  Created by Melchizedek Tetteh on 9/23/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- Flixer/MoviesViewController.swift
+++ Flixer/MoviesViewController.swift
@@ -74,14 +74,30 @@ class MoviesViewController: UIViewController,UITableViewDataSource,UITableViewDe
     
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        print("loading up screen details...")
+        
+        //Find the selected movie
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        
+        let movie = movies[indexPath.row]
+        
+        //pass selected movie to details view controller
+        let detailViewController = segue.destination as! MovieDetailsViewController
+        
+        detailViewController.movie = movie
+        tableView.deselectRow(at: indexPath,animated: true)
+        
     }
-    */
+    
 
 }
